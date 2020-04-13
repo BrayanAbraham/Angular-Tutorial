04328577893f7be576d3181dc7c8a79ef9cebe06
BrayanAbraham <brayanabraham@gmail.com>
2020-04-13 15:32:29 +0530

    Diving Into Components

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  //   template: '<h2>John Doe</h2>',
+  //   styles: [
+  //     `
+  //       h2 {
+  //         color: blue;
+  //       }
+  //     `,
+  //   ],
+})
+export class UserComponent implements OnInit {
+  //properties
+  user: User;
+
+  //Methods
+  constructor() {}
+
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MAS',
+      },
+    };
+  }
+}
