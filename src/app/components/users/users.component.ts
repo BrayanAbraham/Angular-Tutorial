1284f612edf73af76c0d179ea026e5ba79781a3b
BrayanAbraham <brayanabraham@gmail.com>
2020-04-14 20:00:13 +0530

    Component Clean Up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,12 +10,7 @@ export class UsersComponent implements OnInit {
   user: User = {
     firstName: '',
     lastName: '',
-    age: null,
-    address: {
-      street: '',
-      city: '',
-      state: '',
-    },
+    email: '',
   };
   users: User[];
   showExtended: boolean = true;
@@ -30,12 +25,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'John',
         lastName: 'Doe',
-        age: 70,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'MA',
-        },
+        email: 'john@gmail.com',
         isActive: true,
         registered: new Date('01/02/2018 08:30:00'),
         hide: true,
@@ -43,12 +33,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'Kevin',
         lastName: 'Johnson',
-        age: 34,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'MA',
-        },
+        email: 'kevin@gmail.com',
         isActive: false,
         registered: new Date('03/11/2017 06:20:00'),
         hide: true,
@@ -56,12 +41,7 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'Karen',
         lastName: 'Williams',
-        age: 26,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'FL',
-        },
+        email: 'karen@gmail.com',
         isActive: true,
         registered: new Date('11/02/2016 10:30:00'),
         hide: true,
@@ -71,21 +51,16 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  addUser() {
-    this.user.isActive = true;
-    this.user.registered = new Date();
-    this.users.unshift(this.user);
-    this.user = {
-      firstName: '',
-      lastName: '',
-      age: null,
-      address: {
-        street: '',
-        city: '',
-        state: '',
-      },
-    };
-  }
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
+  //   this.user = {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: '',
+  //   };
+  // }
 
   onSubmit(e) {
     e.preventDefault();
