6fb3eb25afb9e70cc1ca22cc6e651aa36906b363
BrayanAbraham <brayanabraham@gmail.com>
2020-04-14 21:35:09 +0530

    HTTP client POST request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { UserService } from './services/user.service';
 import { PostsService } from './services/posts.service';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { PostsService } from './services/posts.service';
     UsersComponent,
     NavbarComponent,
     PostsComponent,
+    PostFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [UserService, PostsService],
